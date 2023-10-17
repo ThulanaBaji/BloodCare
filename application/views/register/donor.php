21bd726a5c8a35d49fdb62ff9a5d1e80f5b96f50
Thulana2006 <thulanabaji@gmail.com>
2023-10-18 01:51:31 +0530

    Improved client-side form validation [production]

--- application/views/register/donor.php
+++ application/views/register/donor.php
@@ -223,8 +223,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
     });
 
     function navigateToFormStep(stepNumber){
-
-   
         if(!isValid(stepNumber))
             return;
 

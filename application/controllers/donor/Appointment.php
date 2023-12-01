59c3ae1260ef7dd5b92222ba09acef0d8b3cb961
Thulana2006 <thulanabaji@gmail.com>
2023-12-01 15:27:08 +0530

    select appointment view component [donor] created

---
+++ application/controllers/donor/Appointment.php
@@ -0,0 +1,38 @@
+<?php
+
+class Appointment extends CI_Controller {
+    private $id;
+    private $role = 'donor';
+
+    public function __construct(){
+		parent::__construct();
+
+		$this->load->library('session');
+        
+        if(!$this->session->has_userdata('user')){
+            $this->session->set_flashdata('error','Please login first to access the page');
+            redirect('login');
+        }
+
+        if($this->session->userdata('user')['role'] != $this->role)
+            show_404();
+
+        $this->id = $this->session->userdata('user')['id'];
+
+		$this->load->database();
+		$this->load->model('donor_model');
+	}
+
+    public function index(){
+        
+        $data['active'] = '2';
+        $data['view'] = 'donor/dashboard/appointment';
+
+        $res = $this->donor_model->getInfo($this->id);
+        foreach ($res as $key => $value)
+            $data[$key] = $value;
+
+        $this->load->view('donor/dashboard', $data);
+    }
+    
+}
--- application/views/donor/dashboard/appointment.php
+++ application/views/donor/dashboard/appointment.php
@@ -1,1 +1,69 @@
-appointment [donor]
+<div class="py-6 px-0 sm:px-6 flex flex-col h-full relative">
+  <div class="p-1 pl-4 mb-3 rounded-xl border max-w-xl bg-gray-50 relative">
+    <div class="flex items-center justify-between w-full">
+        <div class="flex items-center gap-6">
+            <div class="hidden sm:flex w-16 h-16 border items-center rounded-full overflow-hidden">
+                <img src="http://localhost/bloodcare/uploads/donor/profileimages/1701351500kimson-doan-HD8KlyWRYYM-unsplash.jpg" class="object-cover object-center w-16">
+            </div>
+            <div class="flex flex-col text-left text-gray-500 font-semibold gap-3 sm:gap-2">
+                <p class="text-lg sm:text-lg leading-tight">Asiri Hospitals long name</p>
+                <p class="text-xs uppercase flex items-center gap-2 sm:gap-1">
+                    <svg class="w-4 h-4 inline-block text-gray-500 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 16 20">
+                        <path d="M8 0a7.992 7.992 0 0 0-6.583 12.535 1 1 0 0 0 .12.183l.12.146c.112.145.227.285.326.4l5.245 6.374a1 1 0 0 0 1.545-.003l5.092-6.205c.206-.222.4-.455.578-.7l.127-.155a.934.934 0 0 0 .122-.192A8.001 8.001 0 0 0 8 0Zm0 11a3 3 0 1 1 0-6 3 3 0 0 1 0 6Z"/>
+                    </svg> nugegoda, colombo</p>
+            </div>
+        </div>
+        <button class="h-[114px] w-[114px] hover:bg-gray-100 rounded-lg flex items-center justify-center">
+        <svg class="w-6 h-6 text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 8 14">
+            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 13 5.7-5.326a.909.909 0 0 0 0-1.348L1 1"/>
+        </svg>
+        </button>
+    </div>
+
+    <div class="pt-3 pb-3 pr-3 flex justify-center" id="appointments-panel">
+        <div class="rounded-lg bg-white flex flex-grow flex-col justify-center items-center">
+        <div class="flex justify-between flex-grow py-2">
+            <button type="button" class="bg-white rounded-lg text-gray-500 hover:bg-gray-100 hover:text-gray-900 text-lg p-2.5 focus:outline-none focus:ring-2 focus:ring-gray-200">
+                <svg class="w-4 h-4 text-gray-800" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 5H1m0 0 4 4M1 5l4-4"></path>
+                </svg>
+            </button>
+            <span class="text-sm rounded-lg text-gray-900 bg-white font-semibold py-2.5 px-5 ">December 2023</span>
+            <button type="button" class="bg-white rounded-lg text-gray-500 hover:bg-gray-100 hover:text-gray-900 text-lg p-2.5 focus:outline-none focus:ring-2 focus:ring-gray-200">
+                <svg class="w-4 h-4 text-gray-800" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"></path>
+                </svg>
+            </button>
+        </div>
+        <div class="w-full grid grid-cols-2 px-4 mb-4 sm:grid-cols-4 gap-1 hidden">
+            <span class="hover:bg-gray-100 px-2 block flex-1 leading-9 border-0 rounded-lg cursor-pointer text-center font-semibold text-sm text-gray-500">14:00</span>
+            <span class="hover:bg-gray-100 px-2 block flex-1 leading-9 border-0 rounded-lg cursor-pointer text-center font-semibold text-sm text-gray-500">13:00</span>
+            <span class="hover:bg-gray-100 px-2 block flex-1 leading-9 border-0 rounded-lg cursor-pointer text-center font-semibold text-sm text-gray-500">16:00</span>
+        </div>
+        <div class="w-full grid grid-cols-2 px-4 mb-4 sm:grid-cols-4 gap-1">
+            <div>
+                <input type="radio" id="2" class="hidden peer" name="appointment-selection">
+                <label for="2" class="hover:bg-gray-100 px-2 block flex-1 leading-9 border-0 rounded-lg cursor-pointer text-center font-semibold text-sm text-gray-500 peer-checked:bg-gray-100"><span class="">08:00</span></label>
+            </div>
+            <div>
+                <input type="radio" id="3" class="hidden peer" name="appointment-selection">
+                <label for="3" class="hover:bg-gray-100 px-2 block flex-1 leading-9 border-0 rounded-lg cursor-pointer text-center font-semibold text-sm text-gray-500 peer-checked:bg-gray-100"><span class="">08:00</span></label>
+            </div>
+            <span class="hover:bg-gray-100 px-2 block flex-1 leading-9 border-0 rounded-lg cursor-pointer text-center font-semibold text-sm text-gray-500">08:00</span>
+            <span class="hover:bg-gray-100 px-2 block flex-1 leading-9 border-0 rounded-lg cursor-pointer text-center font-semibold text-sm text-gray-500">14:00</span>
+            <span class="hover:bg-gray-100 px-2 block flex-1 leading-9 border-0 rounded-lg cursor-pointer text-center font-semibold text-sm text-gray-500">13:00</span>
+            <span class="hover:bg-gray-100 px-2 block flex-1 leading-9 border-0 rounded-lg cursor-pointer text-center font-semibold text-sm text-gray-500">16:00</span>
+            <span class="hover:bg-gray-100 px-2 block flex-1 leading-9 border-0 rounded-lg cursor-pointer text-center font-semibold text-sm text-gray-500">18:00</span>
+        </div>
+        <div class="w-full grid grid-cols-2 px-4 mb-4 sm:grid-cols-4 gap-1 hidden">
+            <span class="hover:bg-gray-100 px-2 block flex-1 leading-9 border-0 rounded-lg cursor-pointer text-center font-semibold text-sm text-gray-500">08:00</span>
+            <span class="hover:bg-gray-100 px-2 block flex-1 leading-9 border-0 rounded-lg cursor-pointer text-center font-semibold text-sm text-gray-500">14:00</span>
+            <span class="hover:bg-gray-100 px-2 block flex-1 leading-9 border-0 rounded-lg cursor-pointer text-center font-semibold text-sm text-gray-500">13:00</span>
+            <span class="hover:bg-gray-100 px-2 block flex-1 leading-9 border-0 rounded-lg cursor-pointer text-center font-semibold text-sm text-gray-500">16:00</span>
+            <span class="hover:bg-gray-100 px-2 block flex-1 leading-9 border-0 rounded-lg cursor-pointer text-center font-semibold text-sm text-gray-500">18:00</span>
+            <span class="hover:bg-gray-100 px-2 block flex-1 leading-9 border-0 rounded-lg cursor-pointer text-center font-semibold text-sm text-gray-500">18:00</span>
+        </div>
+        </div>
+    </div>
+  </div>
+</div>

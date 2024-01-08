faa84aff9b9c8d5170fe0ad0aecc931dbef78e33
Thulana2006 <thulanabaji@gmail.com>
2024-01-08 18:45:21 +0530

    Modified mobile view in camp card [donor, hospital]

--- application/helpers/hospital/Loadorganizedcamps_helper.php
+++ application/helpers/hospital/Loadorganizedcamps_helper.php
@@ -80,28 +80,28 @@ if (!function_exists('loadOrganizedCamps')) {
             <div class="py-1 flex justify-center hidden" id="camp-panel-$id">
                 <div class="w-full pt-4 pr-3">
                     <div class="bg-gray-100 p-3 rounded-lg">
-                        <div class="flex items-center gap-3">
-                            <div class="text-gray-500 font-bold text-xs rounded w-20 p-1 px-2">Time</div>
-                            <div class="text-gray-500 font-semibold text-sm">$time</div>
+                        <div class="flex sm:flex-row flex-col sm:items-center sm:gap-3">
+                            <div class="text-gray-500 font-semibold sm:font-bold text-sm rounded w-20 sm:p-1 sm:px-2">Time</div>
+                            <div class="text-gray-500 sm:font-semibold text-sm">$time</div>
                         </div>
-                        <div class="flex items-center gap-3 mt-2">
-                            <div class="text-gray-500 font-bold text-xs rounded w-20 p-1 px-2">Location</div>
-                            <div class="text-gray-500 font-semibold text-sm underline">
+                        <div class="flex sm:flex-row flex-col sm:items-center sm:gap-3 mt-3">
+                            <div class="text-gray-500 font-semibold sm:font-bold text-sm rounded w-20 sm:p-1 sm:px-2">Location</div>
+                            <div class="text-gray-500 sm:font-semibold text-sm underline">
                                 <a href="$pin"><u>$address</u>
                                 <svg class="w-3 h-3 inline-block ml-1 text-gray-500 " aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
                                     <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 11v4.833A1.166 1.166 0 0 1 13.833 17H2.167A1.167 1.167 0 0 1 1 15.833V4.167A1.166 1.166 0 0 1 2.167 3h4.618m4.447-2H17v5.768M9.111 8.889l7.778-7.778"/>
                                 </svg></a>
                             </div>
                         </div>
-                        <div class="flex items-center gap-3 mt-2">
-                            <div class="text-gray-500 font-bold text-xs rounded w-20 p-1 px-2">Organizer</div>
-                            <div class="text-gray-500 font-semibold text-sm">
+                        <div class="flex sm:flex-row flex-col sm:items-center sm:gap-3 mt-3">
+                            <div class="text-gray-500 font-semibold sm:font-bold text-sm rounded w-20 sm:p-1 sm:px-2">Organizer</div>
+                            <div class="text-gray-500 sm:font-semibold text-sm">
                                 $organizer
                             </div>
                         </div>
-                        <div class="flex items-center gap-3 mt-2">
-                            <div class=" text-gray-500 font-bold text-xs rounded w-20 p-1 px-2">With</div>
-                            <div class="camp-details-organizer text-gray-500 font-semibold text-sm">
+                        <div class="flex sm:flex-row flex-col sm:items-center sm:gap-3 mt-3">
+                            <div class="text-gray-500 font-semibold sm:font-bold text-sm rounded w-20 sm:p-1 sm:px-2">With</div>
+                            <div class="camp-details-organizer text-gray-500 sm:font-semibold text-sm">
                                 $with
                             </div>
                         </div>

94460ccf7bbeee7197f4319a5045cfe7b84e266d
Thulana2006 <thulanabaji@gmail.com>
2024-02-08 13:37:43 +0530

    dashboard charts

--- application/views/hospital/dashboard/dashboard.php
+++ application/views/hospital/dashboard/dashboard.php
@@ -1,1 +1,181 @@
-dashboard [hospital]
+<div class="w-full h-full p-3 md:p-6 flex flex-col items-center relative">
+    <div class="absolute z-30 top-10 left-1/2 -translate-x-1/2 max-w-xs">
+        <div class="mt-3 alert alert-success flex items-center p-2 px-3 text-sm text-green-800 rounded bg-green-200" style="display:none;">
+        <svg class="flex-shrink-0 inline w-4 h-4 me-3 " aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+            <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z"></path>
+        </svg>
+        <div id="alert-top-success-text"></div>
+        </div>
+        <div class="mt-3 alert alert-error flex items-center p-2 px-3 text-sm text-red-900 rounded bg-red-300" style="display:none;">
+        <svg class="flex-shrink-0 inline w-4 h-4 me-3 " aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+            <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z"></path>
+        </svg>
+        <div id="alert-top-error-text"></div>
+        </div>
+    </div>
+    
+    <div class="w-full h-full">
+        <!-- row -->
+        <div class="flex gap-3 flex-wrap"> 
+            <div class="rounded-lg border border-cyan-600 bg-gray-50 flex flex-col w-fit p-4">
+                <div class="flex gap-6 items-center ">
+                    <div class="flex items-center justify-center rounded-full w-16 h-16 bg-cyan-700">
+                        <svg class="fill-cyan-100 w-6 h-6" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" 
+                                viewBox="0 0 512 512" xml:space="preserve">
+                            <g>
+                                <g>
+                                    <path d="M167.141,131.229c-5.424,1.593-9.902,2.337-14.098,2.337c-27.619,0-50.087-22.468-50.087-50.087
+                                        c0-20.788,12.631-39.081,32.174-46.603c14.564-5.604,14.125-26.493-0.734-31.429C123.353,1.783,113.071,0,102.957,0
+                                        C47.723,0,2.783,44.94,2.783,100.174c0,55.234,44.94,100.174,100.174,100.174c33.044,0,63.945-16.326,82.674-43.668
+                                        C194.445,143.806,182.243,126.828,167.141,131.229z"/>
+                                </g>
+                            </g>
+                            <g>
+                                <g>
+                                    <path d="M492.522,144.696h-16.696V128c0-9.223-7.473-16.696-16.696-16.696c-9.223,0-16.696,7.473-16.696,16.696v16.696h-16.696
+                                        c-9.223,0-16.696,7.473-16.696,16.696c0,9.223,7.473,16.696,16.696,16.696h16.696v16.696c0,9.223,7.473,16.696,16.696,16.696
+                                        c9.223,0,16.696-7.473,16.696-16.696v-16.696h16.696c9.223,0,16.696-7.473,16.696-16.696
+                                        C509.217,152.169,501.744,144.696,492.522,144.696z"/>
+                                </g>
+                            </g>
+                            <g>
+                                <g>
+                                    <path d="M392.348,33.391h-16.696V16.696C375.652,7.473,368.179,0,358.957,0c-9.223,0-16.696,7.473-16.696,16.696v16.696h-16.696
+                                        c-9.223,0-16.696,7.473-16.696,16.696s7.473,16.696,16.696,16.696h16.696v16.696c0,9.223,7.473,16.696,16.696,16.696
+                                        c9.223,0,16.696-7.473,16.696-16.696V66.783h16.696c9.223,0,16.696-7.473,16.696-16.696S401.57,33.391,392.348,33.391z"/>
+                                </g>
+                            </g>
+                            <g>
+                                <g>
+                                    <path d="M492.522,378.435c-9.223,0-16.696,7.473-16.696,16.696v47.205L272.462,151.815c-6.542-9.229-20.474-9.711-27.359,0
+                                        L41.739,442.336V395.13c0-9.223-7.473-16.696-16.696-16.696c-9.223,0-16.696,7.473-16.696,16.696v100.174
+                                        c0,9.366,7.721,16.696,16.696,16.696c46.81,0,420.628,0,467.478,0c8.894,0,16.696-7.337,16.696-16.696V395.13
+                                        C509.217,385.908,501.744,378.435,492.522,378.435z M275.478,478.609V372.237c50.601,96.367,35.347,67.316,55.854,106.371H275.478
+                                        z"/>
+                                </g>
+                            </g>
+                        </svg>
+                    </div>
+                    <div class="flex items-baseline gap-1">
+                        <p class="text-3xl  text-gray-600">13</p>
+                        <p class="text-sm font-semibold font-sans text-gray-600">Total camps</p>
+                    </div>
+                </div>
+
+                <div id="area-chart"></div>
+            </div>
+
+            <div class="rounded-lg border border-emerald-600 bg-gray-50 flex flex-col w-fit p-4">
+                <div class="flex gap-6 items-center ">
+                    <div class="flex items-center justify-center rounded-full w-16 h-16 bg-emerald-700">
+                        <svg class="fill-emerald-100 w-6 h-6" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24">
+                            <path fill-rule="evenodd" d="M9 2.2V7H4.2l.4-.5 3.9-4 .5-.3Zm2-.2v5a2 2 0 0 1-2 2H4v11c0 1.1.9 2 2 2h12a2 2 0 0 0 2-2V4a2 2 0 0 0-2-2h-7ZM8 16c0-.6.4-1 1-1h6a1 1 0 1 1 0 2H9a1 1 0 0 1-1-1Zm1-5a1 1 0 1 0 0 2h6a1 1 0 1 0 0-2H9Z" clip-rule="evenodd"/>
+                        </svg>
+                        
+                    </div>
+                    <div class="flex items-baseline gap-1">
+                        <p class="text-3xl  text-gray-600">89</p>
+                        <p class="text-sm font-semibold font-sans text-gray-600">Total appointments</p>
+                    </div>
+                </div>
+
+                <div id="area-chart-2"></div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<script src="<?= base_url('node_modules/apexcharts/dist/apexcharts.min.js') ?>"></script>
+
+<script>
+    $(document).ready(function(){
+        var success = <?= isset($success) ? "'".$success."'" : '""' ?>;
+        var error =   <?= isset($error)   ? "'".$error."'"   : '""' ?>;
+
+        if(success != ''){
+            $('.alert-success').fadeIn(200).delay(3000).fadeOut(200);
+            $('#alert-top-success-text').text(success);
+        }
+        if(error != ''){
+            $('.alert-error').fadeIn(200).delay(3000).fadeOut(200);
+            $('#alert-top-error-text').text(error);
+        }
+
+        loadChart('area-chart', "#0e7490", "#cffafe");
+        loadChart('area-chart-2', "#047857", "#d1fae5");
+    });
+
+    function loadChart(id, color, gradcolor) {
+        let options = {
+        chart: {
+            height: "50%",
+            maxWidth: "100%",
+            type: "area",
+            fontFamily: "Inter, sans-serif",
+            dropShadow: {
+            enabled: false,
+            },
+            toolbar: {
+            show: false,
+            },
+        },
+        tooltip: {
+            enabled: true,
+            x: {
+            show: false,
+            },
+        },
+        fill: {
+            type: "gradient",
+            gradient: {
+            opacityFrom: 0.55,
+            opacityTo: 0,
+            shade: gradcolor,
+            gradientToColors: [gradcolor],
+            },
+        },
+        dataLabels: {
+            enabled: false,
+        },
+        stroke: {
+            width: 6,
+        },
+        grid: {
+            show: false,
+            strokeDashArray: 4,
+            padding: {
+            left: 2,
+            right: 2,
+            top: 0
+            },
+        },
+        series: [
+            {
+            name: "New users",
+            data: [6500, 6418, 6456, 6526, 6356, 6456],
+            color: color,
+            },
+        ],
+        xaxis: {
+            categories: ['01 February', '02 February', '03 February', '04 February', '05 February', '06 February', '07 February'],
+            labels: {
+            show: false,
+            },
+            axisBorder: {
+            show: false,
+            },
+            axisTicks: {
+            show: false,
+            },
+        },
+        yaxis: {
+            show: false,
+        },
+        }
+
+        if (document.getElementById(id) && typeof ApexCharts !== 'undefined') {
+        const chart = new ApexCharts(document.getElementById(id), options);
+        chart.render();
+        }
+    }
+</script>
